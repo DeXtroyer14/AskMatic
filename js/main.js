109eaf43500c4bce0b0219c88c64349e7461dd86
Joel Garrido Martínez <joelgarridomartinez@gmail.com>
2022-03-04 18:36:40 +0100

    Añadida funcionalidad creación de preguntas

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ function AlertaEliminarDatos() {
 function AlertaInputError() {
   var InputError = new bootstrap.Modal(document.getElementById('InputError'))
   InputError.toggle()
-  console.log("Error input")
 }
 function AlertaCategoriaExistente() {
   var AlertaCategoriaExistente = new bootstrap.Modal(document.getElementById('AlertaCategoriaExistente'))
@@ -31,7 +30,7 @@ function CrearFormulario() {
   if (localStorage.getItem("Categorias") === null) {
     FormularioAñadirCategoria()
   } else {
-    MenuCrearPreguntas()
+    MenuCrearYSeleccionarCategoria()
   }
 }
 
@@ -49,9 +48,7 @@ function PrimeraLetraMayuscula(string) {
 }
 
 function MandarCategoria() {
-  console.log("Añadir categoría")
   var categoria = document.getElementById('categoria').value;
-  console.log(categoria)
   if (categoria) {
     AñadirCategoriaLocalStorage(PrimeraLetraMayuscula(categoria))
   } else {
@@ -61,38 +58,100 @@ function MandarCategoria() {
 
 // Crear formulario para añadir categoría
 function FormularioAñadirCategoria() {
-  document.getElementById("formulario").innerHTML = '<div class="card mb-4 rounded-3 shadow-sm"> <div class="card-header py-3"> <h4 class="my-0 fw-normal">Crea tu primera categoría</h4> </div> <div class="card-body"> <form class="" action="crear.html" method="post"> <input type="text" id="categoria" class="form-control" aria-label="Username" aria-describedby="basic-addon1"><div class="pricing-header p-3 pb-md-4 mx-auto text-center"></div><button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="MandarCategoria()">Crear</button> </form> </div> </div>';
+  document.getElementById("formulario").innerHTML = '<div class="card mb-4 rounded-3 shadow-sm"> <div class="card-header py-3"> <h4 class="my-0 fw-normal">Crea tu primera categoría</h4> </div> <div class="card-body"><input type="text" id="categoria" class="form-control" aria-label="Username" aria-describedby="basic-addon1"><div class="pricing-header p-3 pb-md-4 mx-auto text-center"></div><button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="MandarCategoria()">Crear</button></div> </div>';
 }
 
-function MenuCrearPreguntas() {
-  var MenuParte1 = '<div class="row g-3"> <div class="col-md"> <div class="form-floating"> <input type="text" class="form-control" id="categoria" placeholder="" value="" aria-describedby="button-addon2"> <label for="categoria">Crear nueva categoría</label> </div> </div> <div class="col-md"> <div class="form-floating"> <select class="form-select" id="CategoriaSeleccionada" aria-label="Floating label select">'
+function MenuCrearYSeleccionarCategoria() {
+  var MenuParte1 = '<div class="row g-3"> <div class="col-md"> <div class="form-floating"> <input type="text" class="form-control" id="categoria" placeholder="" value="" aria-describedby="button-addon2"> <label for="categoria">Crear nueva categoría</label> </div> </div> <div class="col-md"> <div class="form-floating"> <select class="form-select" id="CategoriaSeleccionada" onchange="MenuListaPreguntas()" aria-label="Floating label select">'
   var MenuParte2 = '</select> <label for="CategoriaSeleccionada">Selecciona una categoría</label> </div> </div> </div>'
   var listaCategorias = null
 
 
+
   var ArrayCategorias = JSON.parse(localStorage.getItem("Categorias"))
-  console.log(ArrayCategorias)
   ArrayCategorias.forEach(ListarCategorias);
 
   document.getElementById("formulario").innerHTML = MenuParte1 + listaCategorias + MenuParte2;
 
+  MenuListaPreguntas()
+
   function ListarCategorias(item, index) {
-    if (listaCategorias === null){
-      listaCategorias = '<option value="'+index+'">'+item+'</option>'
-    }else{
-      listaCategorias = listaCategorias.concat('<option value="',index,'">',item,'</option>')
+    if (listaCategorias === null) {
+      listaCategorias = '<option value="' + index + '">' + item + '</option>'
+    } else {
+      listaCategorias = listaCategorias.concat('<option value="', index, '">', item, '</option>')
     }
   }
 
   //Al presionar enter mandamos la nueva categoría a LocalStorage
   document.getElementById("categoria").addEventListener("keyup", function (event) {
     if (event.code === "Enter") {
-      console.log(event.key)
       MandarCategoria();
-      CrearFormulario()
+      CrearFormulario();
       return false;
     }
   });
+
+
+}
+
+
+function MenuListaPreguntas() {
+  var IDCategoriaSeleccionada = document.getElementById("CategoriaSeleccionada").value
+  var NombreCategoriaSeleccionada = JSON.parse(localStorage.getItem("Categorias"))[IDCategoriaSeleccionada]
+
+  if (localStorage.getItem("Preguntas-" + NombreCategoriaSeleccionada) === null) {
+    PrimeraPregunta();
+  } else {
+    document.getElementById("ListaPreguntas").innerHTML = ""
+    var ArrayPreguntas = JSON.parse(localStorage.getItem("Preguntas-" + NombreCategoriaSeleccionada))
+    var ArrayRespuestas = JSON.parse(localStorage.getItem("Respuestas-" + NombreCategoriaSeleccionada))
+    for (let i = 0; i < ArrayPreguntas.length; i++) {
+      AñadirPreguntaExtra(ArrayPreguntas[i], ArrayRespuestas[i])
+    }
+  }
+
+  document.getElementById("BotonesPreguntas").innerHTML = '<div class="row g-2"><div class="col-md"><button type="button" class="w-100 btn btn-lg btn-outline-secondary" onclick="AñadirPreguntaExtra()">Nueva pregunta</button></div><div class="col-md"><button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="GuardarPreguntas()">Guardar</button></div></div>';
+}
+
+function AñadirPreguntaExtra(pregunta, respuesta) {
+  if (pregunta == null) {
+    var pregunta = ""
+  }
+  if (respuesta == null) {
+    var respuesta = ""
+  }
+  const TimeoutAñadirPreguntaExtra = setTimeout(function () { document.getElementById("ListaPreguntas").insertAdjacentHTML('beforeend','<div class="card mb-4 rounded-3 shadow-sm"> <div class="card-header py-3"> <div class="form-floating mb-3"> <input type="text" class="form-control" name="pregunta[]" placeholder="Pregunta" value="' + pregunta + '"> <label for="floatingInput">Pregunta</label> </div> </div> <div class="card-body"><div class="form-floating mb-3"> <input type="text" class="form-control" name="respuesta[]" placeholder="Respuesta" value="' + respuesta + '"> <label for="floatingInput">Respuesta</label> </div></div> </div>') }, 0);
+  //document.getElementById("ListaPreguntas").innerHTML += '<div class="card mb-4 rounded-3 shadow-sm"> <div class="card-header py-3"> <div class="form-floating mb-3"> <input type="text" class="form-control" name="pregunta[]" placeholder="Pregunta" value="' + pregunta + '"> <label for="floatingInput">Pregunta</label> </div> </div> <div class="card-body"><div class="form-floating mb-3"> <input type="text" class="form-control" name="respuesta[]" placeholder="Respuesta" value="' + respuesta + '"> <label for="floatingInput">Respuesta</label> </div></div> </div>';
+}
+
+function PrimeraPregunta() {
+  document.getElementById("ListaPreguntas").innerHTML = '<div class="card mb-4 rounded-3 shadow-sm"> <div class="card-header py-3"> <div class="form-floating mb-3"> <input type="text" class="form-control" name="pregunta[]" placeholder="Pregunta"> <label for="floatingInput">Pregunta</label> </div> </div> <div class="card-body"><div class="form-floating mb-3"> <input type="text" class="form-control" name="respuesta[]" placeholder="Respuesta"> <label for="floatingInput">Respuesta</label> </div></div> </div>';
+}
+
+
+function GuardarPreguntas() {
+  var IDCategoriaSeleccionada = document.getElementById("CategoriaSeleccionada").value
+  var NombreCategoriaSeleccionada = JSON.parse(localStorage.getItem("Categorias"))[IDCategoriaSeleccionada]
+
+  var Preguntas = document.getElementsByName('pregunta[]');
+  var Respuestas = document.getElementsByName('respuesta[]');
+
+  var p = "";
+  var r = "";
+
+  for (var i = 0; i < Preguntas.length; i++) {
+    var a = Preguntas[i];
+    var b = Respuestas[i];
+    p = p + a.value + ';';
+    r = r + b.value + ';';
+  }
+
+  var ArrayPreguntas = p.slice(0, -1).split(";");
+  var ArrayRespuestas = r.slice(0, -1).split(";");
+
+  localStorage.setItem("Preguntas-" + NombreCategoriaSeleccionada, JSON.stringify(ArrayPreguntas))
+  localStorage.setItem("Respuestas-" + NombreCategoriaSeleccionada, JSON.stringify(ArrayRespuestas))
 
 }
 
@@ -136,12 +195,12 @@ function ImportarArchivo() {
   fileReader.onload = function (fileLoadedEvent) {
     var textFromFileLoaded = fileLoadedEvent.target.result;
     var datos = JSON.parse(textFromFileLoaded);
-    console.log(datos)
     Object.keys(datos).forEach(function (k) {
       localStorage.setItem(k, datos[k]);
     });
 
   };
 
-  fileReader.readAsText(fileToLoad, "UTF-8");
+  fileReader.readAsText(fileToLoad, "UTF-8")
+
 }
